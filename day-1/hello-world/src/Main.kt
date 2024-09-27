3ab2ec42a37b7913eddec57bec20d6680b1fa356
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-27 14:41:28 +0700

    story-1

--- day-1/hello-world/src/Main.kt
+++ day-1/hello-world/src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    val score = arrayOf(59, 69, 79, 89, 99)
+    for (x in score) {
+        if (x < 60) {
+            println("F")
+        } else if (x < 70) {
+            println("D")
+        } else if (x < 80) {
+            println("C")
+        } else if (x < 90) {
+            println("B")
+        } else {
+            println("A")
+        }
+    }
 }
