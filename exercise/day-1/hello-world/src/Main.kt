b07bb657906b9273a29936a351e85fc9050fc9b6
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-27 15:21:07 +0700

    story-3

--- exercise/day-1/hello-world/src/Main.kt
+++ exercise/day-1/hello-world/src/Main.kt
@@ -16,15 +16,28 @@ fun main() {
 //        }
 //    }
 
-    val experience = arrayOf(1, 3, 6)
-    val salary = 10000
-    for (x in experience) {
-        if (x < 2) {
-            println(0)
-        } else if (x <= 5) {
-            println(salary * 0.1)
+//    val experience = arrayOf(200, 150, 50, 40)
+//    val salary = 10000
+//    for (x in experience) {
+//        if (x < 2) {
+//            println(0)
+//        } else if (x <= 5) {
+//            println(salary * 0.1)
+//        } else {
+//            println(salary * 0.2)
+//        }
+//    }
+
+    val totalSpent = arrayOf(300, 150, 50, 40)
+    for (x in totalSpent) {
+        if (x < 50) {
+            println(x)
+        } else if (x <= 100) {
+            println(x * 0.9)
+        } else if (x <= 200) {
+            println(x * 0.8)
         } else {
-            println(salary * 0.2)
+            println(x * 0.7)
         }
     }
 
